b40a7d4cac7ca993880abb26d8f3688dbaf98b66
kristofszk <kristofszk@gmail.com>
2018-11-11 20:33:08 +0200

    Indented code, added gitignore, removed ActionListener

--- Gui/src/main/java/lab1/MyFrame.java
+++ Gui/src/main/java/lab1/MyFrame.java
@@ -18,7 +18,7 @@ import javax.swing.table.TableModel;
 import Database.PersonProvider;
 import Model.Person;
 
-public class MyFrame extends JFrame implements ActionListener{
+public class MyFrame extends JFrame {
 	private static JTable table= null;
 	private static JButton button=new JButton("Hide");
 	private JPanel panel=new JPanel();
@@ -26,45 +26,38 @@ public class MyFrame extends JFrame implements ActionListener{
 	private JFrame frame = new JFrame("Gui");
 	private static ArrayList<Person> users=new ArrayList<Person>();
 
-	
-	public MyFrame() {
-		
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fillTable();
-        addComponentsToPane(frame.getContentPane());
-        
-        
-        frame.pack();
-        frame.setVisible(true);
-		
+
+	public MyFrame() {	
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fillTable();
+		addComponentsToPane(frame.getContentPane());
+		frame.pack();
+		frame.setVisible(true);
 	}
-	
+
 	public static void addComponentsToPane(Container pane) {
-        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        table.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            	System.out.println(button.getText());
-            	if(button.getText().contains("Hide")) {
-            		table.setVisible(false);
-            		button.setText("Show");
-            		
-            	}else{
-            		
-            		table.setVisible(true);
-            		button.setText("Hide");
-            		
-            	}
-            	fillTable();
-            }
-        });
-        pane.add(table);
-        pane.add(button);
-    }
-	
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		table.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(button.getText());
+				if(button.getText().contains("Hide")) {
+					table.setVisible(false);
+					button.setText("Show");
+				}else{
+					table.setVisible(true);
+					button.setText("Hide");
+				}
+				fillTable();
+			}
+		});
+		pane.add(table);
+		pane.add(button);
+	}
+
 	public static void fillTable() {
-		
+
 		PersonProvider p =new PersonProvider();
 		try {
 			users=p.getPersons();
@@ -72,10 +65,8 @@ public class MyFrame extends JFrame implements ActionListener{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		int rc=0;
-		for(Person pers :users) {rc++;}
-		DefaultTableModel model = new DefaultTableModel(rc+1,4 );
-		
+
+		DefaultTableModel model = new DefaultTableModel(1,4 );
 		for(Person pers :users )
 		{
 			System.out.println(pers.getEmail());
@@ -85,11 +76,4 @@ public class MyFrame extends JFrame implements ActionListener{
 		table = new JTable(model);
 		table.setVisible(true);
 	}
-
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
 }
